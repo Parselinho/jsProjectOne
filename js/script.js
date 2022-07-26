d5693bfd57d1734889bba987bb213a164489caaa
Parselinho <102391821+Parselinho@users.noreply.github.com>
2022-07-26 17:15:57 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,49 +1,45 @@
+// quotes array 
 let quotes = 
 [
+  // objects inside array : 
   {
     quote: "Tell me and I forget, teach me and I may remember, involve me and I learn",
     source: "Xun Kuang",
     year: -454 ,
-    citation: "one of the quotes that has been added to the Xunzi old book",
-    tags: "study"
+    tags: "#study"
   },
   {
     quote: "Learning never exhausts the mind",
     source: "Leonardo da Vinci",
     year: 1499 ,
-    citation: "Leonardo answered a friend question, with this answer",
-    tags: "study"
+    tags: "#study"
   },
   {
     quote: "I Am Always Ready to Learn, Although I Do Not Always Like Being Taught",
     source: "Winston Churchill",
     year: 1952 ,
     citation: "Speaking in the House of Commons in London",
-    tags: "study"
   },
   {
     quote: "In life all good things pass. But even the bad things, they also pass",
     source: "Menachem Begin",
     year: 1967 ,
-    citation: "ex-prime minister of Israel, tallking to the people of Israel in the middle of a war",
-    tags: "inspiration"
+    tags: "#inspiration"
   },
   {
     quote: "The world is nothing but a large children's room, full of various amusements: wealth, honor, health, life. You should learn from the children: you came across one toy, cried for a moment, and immediately took another toy and rested your soul",
     source: "Ze'ev Jabotinsky",
     year: 1921 ,
-    citation: "tallking in the World Zionist Congress in Czechoslovakia",
-    tags: "inspiration"
+    tags: "#inspiration"
   },
   {
     quote: "Life is like riding a bicycle: you have to move forward all the time without stopping, so as not to lose your balance",
     source: "Albert Einsiten",
     year: 1930 ,
     citation: "Writing to his son Eduard",
-    tags: "inspiration"
   }
 ];
-
+ // return random object from the quotes array
 function getRandomQuote() {
   let randomNumber = Math.floor(Math.random() * (quotes.length));
   return quotes[randomNumber];
@@ -70,5 +66,12 @@ function printQuote() {
    // printing the thing we wrote above
    document.getElementById('quote-box').innerHTML = htmlString; 
 }
+// refresh the printquote every 15 seconds
+setInterval(printQuote, 15000);
 
+// changing color while clicking the button
+function changeColor() {
+  document.body.style.backgroundColor = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+}
+// event listener for pressing the button (false to make bubbling phase [ after get to all children of elem])
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
